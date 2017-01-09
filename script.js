ee6572569a36ef2d15b8c476e2bf3ecd7895fdf7
Christoph Matthies <christoph.matthies@gmail.com>
2017-01-09 01:43:03 +0100

    Fix JS for Chrome.

--- script.js
+++ script.js
@@ -8,15 +8,15 @@
 
 function main(){
 	var timelineBlocks = document.querySelectorAll('.timeline > li');
+	// http://stackoverflow.com/questions/28633221/document-body-scrolltop-firefox-returns-0-only-js
 	var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
 	var windowHeight = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
 
 	// Hide the parts of the timeline below the fold
 	Array.prototype.forEach.call(timelineBlocks, function(el, i){
-		var rect = el.getBoundingClientRect();
-		var offsetTop = rect.top + document.body.scrollTop;
+		var offsetTop = el.getBoundingClientRect().top + scrollTop;
 		if (offsetTop > scrollTop + windowHeight * 0.9) {
-			var timelineParts = el.querySelectorAll('.timeline-panel, .timeline-badge');
+			var timelineParts = el.querySelectorAll('.tl-panel, .tl-badge');
 			Array.prototype.forEach.call(timelineParts, function(part, i){
 				part.classList.add('hidden');
 			});
@@ -26,10 +26,11 @@ function main(){
 	// Show parts of the timeline when they enter the viewport
 	window.addEventListener('scroll', function(){
 		Array.prototype.forEach.call(timelineBlocks, function(el, i){
-			var rect = el.getBoundingClientRect();
-			var offsetTop = rect.top + document.body.scrollTop;
-			if (offsetTop <= scrollTop + windowHeight * 0.9 && el.querySelectorAll('.timeline-panel')[0].classList.contains('hidden')) {
-				var timelineParts = el.querySelectorAll('.timeline-panel, .timeline-badge');
+			var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+			var offsetTop = el.getBoundingClientRect().top + scrollTop;
+			var windowHeight = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
+			if (offsetTop <= scrollTop + windowHeight * 0.9 && el.querySelector('.tl-panel').classList.contains('hidden')) {
+				var timelineParts = el.querySelectorAll('.tl-panel, .tl-badge');
 				Array.prototype.forEach.call(timelineParts, function(part, i){
 					part.classList.remove('hidden');
 					part.classList.add('bounce-in');
